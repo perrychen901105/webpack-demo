2a85349fb874f12aa6ea14f61489e6687dab1e9c
perry.z.chen@pwc.com <chenzhipeng901105@gmail.com>
2018-11-02 17:10:56 +0800

    add code

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,32 @@
-const path = require("path");
-const webpack = require("webpack");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  devServer: {
-    watchOptions: {
-      // Delay the rebuild after the first change
-      aggregateTimeout: 300,
+const parts = require("./webpack.parts");
 
-      // Poll using interval (in ms, accepts boolean too)
-      poll: 1000,
-    },
+const commonConfig = merge([
+  {
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack demo",
+      }),
+    ],
   },
-  plugins: [
-    // Ignore node_modules so CPU usage with poll
-    // watching drops significantly.
-    new webpack.WatchIgnorePlugin([
-      path.join(__dirname, "node_modules")
-    ]),
-  ],
+]);
+
+const productionConfig = merge([]);
+
+const developmentConfig = merge([
+  parts.devServer({
+    // Customize host/port here if needed
+    host: process.env.HOST,
+    port: process.env.PORT,
+  }),
+]);
+
+module.exports = mode => {
+  if (mode === "production") {
+    return merge(commonConfig, productionConfig, { mode });
+  }
+
+  return merge(commonConfig, developmentConfig, { mode });
 };
-
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-// module.exports = {
-//     devServer: {
-//         // Display only errors to reduce the amount of output.
-//         stats: "errors-only",
-    
-//         // Parse host and port from env to allow customization.
-//         //
-//         // If you use Docker, Vagrant or Cloud9, set
-//         // host: options.host || "0.0.0.0";
-//         //
-//         // 0.0.0.0 is available to all network devices
-//         // unlike default `localhost`.
-//         host: process.env.HOST, // Defaults to `localhost`
-//         port: process.env.PORT, // Defaults to 8080
-//         open: true, // Open the page in browser
-//         overlay: true,
-//     },
-//     plugins: [
-//         new HtmlWebpackPlugin({
-//             title: "Webpack demo",
-//         }),
-//     ],
-// };
